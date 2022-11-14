4abcb0524ab5e54c680b5a519e4102571dacd93f
SCNnuts <114662407+SCNnuts@users.noreply.github.com>
2022-11-14 14:39:25 +0100

    Update VA4.py

--- VA4.py
+++ VA4.py
@@ -323,7 +323,6 @@ elif pages == 'Visualisaties':
     st.pyplot(fig3)
 elif pages == 'Wereld Kaart':
     option = st.selectbox('Welke kaart?', ('Choropleth','Heatmap')
-    if option == 'Choropleth':
     st_data = st_folium(m)                 
 elif option == 'Heatmap':
 elif pages == 'Einde':
