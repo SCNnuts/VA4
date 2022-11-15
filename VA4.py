69d5dc9a22a56ea3347829aee9da5762ca1f197d
SCNnuts <114662407+SCNnuts@users.noreply.github.com>
2022-11-15 11:30:55 +0100

    Update VA4.py

--- VA4.py
+++ VA4.py
@@ -145,6 +145,16 @@ plt.show()
 
 # In[16]:
 
+unemp3 = unemp_long.loc[(unemp_long['Country Name'] == 'Netherlands')]
+unemp3
+
+fig4 = sns.scatterplot(x="Year", y="Unemployment Rate", data=unemp3)
+plt.title("Unemployment Rate", size=15, color="blue")
+plt.xlabel("Year")
+plt.xticks(rotation = 90) 
+plt.ylabel("Unemployment Rate")
+#plt.show()
+
 
 #gk = unemp_long.groupby('Year')
 #gk.get_group('1991')
@@ -329,7 +339,7 @@ elif pages == 'Datasets':
     st.dataframe(data=happiness_merge, use_container_width=False)
 elif pages == 'Visualisaties':
     st.subheader("Hier worden de visualisaties weergegeven die wij hebben opgesteld.")
-    option = st.selectbox('Welke visualisatie zou u graag willen bekijken, klik om de balk om de mogelijkheden te zien', ('Plot unemployment per continent','Bar plots (laagste/hoogste employment)'))
+    option = st.selectbox('Welke visualisatie zou u graag willen bekijken, klik om de balk om de mogelijkheden te zien', ('Plot unemployment per continent','Bar plots (laagste/hoogste employment)','Scatterplot van Nederland))
     if option == 'Plot unemployment per continent':
           st.markdown("Hieronder wordt een grafiek weergegeven met data wereldwijd over de unemployment rate.")
           st.plotly_chart(fig, use_container_width=True)
@@ -337,6 +347,9 @@ elif pages == 'Visualisaties':
           st.markdown("Hieronder wordt een barplot weergegeven met een overzicht van de 10 hoogste unemployment rate landen wereldwijd.")
           st.pyplot(fig1) 
           st.pyplot(fig3)
+    elif option == 'Bar plots (laagste/hoogste employment)':
+          st.markdown("Hieronder wordt een barplot weergegeven met een overzicht van de 10 hoogste unemployment rate landen wereldwijd.")
+          st.pyplot(fig4)                                                                                                                      
 elif pages == 'Wereld Kaart':
     option = st.selectbox('Welke kaart?', ('Choropleth','Heatmap'))
     if option == 'Choropleth':
